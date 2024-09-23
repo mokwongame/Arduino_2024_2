e9877c9c907cfdd2cf4f21f7aff6f3813e1c66c2
mokwongame <ictplusud@gmail.com>
2024-09-23 14:46:13 +0900

    3색 LED 초기화

--- TestAruino/TestAruino.ino
+++ TestAruino/TestAruino.ino
@@ -7,9 +7,20 @@
 6. 그 다음 버튼 누르면 RGB off
  */
 
+#define PORT_R  (2)
+#define PORT_G  (3)
+#define PORT_B  (4)
+
 void setup() {
   // put your setup code here, to run once:
-
+  // digital port를 출력(output)으로 사용하는 모드(mode)로 선택
+  pinMode(PORT_R, OUTPUT);
+  pinMode(PORT_G, OUTPUT);
+  pinMode(PORT_B, OUTPUT);
+  // digital port에 출력을 LOW(접지, 0V)로 쓰기
+  digitalWrite(PORT_R, LOW);
+  digitalWrite(PORT_G, LOW);
+  digitalWrite(PORT_B, LOW);
 }
 
 void loop() {
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "Arduino 실습 과제 설명"
+git commit -m "3색 LED 초기화"
 git push -u origin master
