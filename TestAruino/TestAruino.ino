1263abe4b4dd5d6c82e07597eebe19923b22b89d
mokwongame <ictplusud@gmail.com>
2024-09-23 14:20:28 +0900

    Arduino 실습 과제 설명

--- TestAruino/TestAruino.ino
+++ TestAruino/TestAruino.ino
@@ -1,3 +1,11 @@
+/* Push button으로 3색 LED 제어
+1. 처음에는 LED가 모두 off
+2. 버튼 누르면 R on
+3. 그 다음 버튼 누르면 G on, R off
+4. 그 다음 버튼 누르면 B on, G off
+5. 그 다음 버튼 누르면 RGB on
+6. 그 다음 버튼 누르면 RGB off
+ */
 
 void setup() {
   // put your setup code here, to run once:
--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 chcp 65001
 git add --all
-git commit -m "first commit"
+git commit -m "Arduino 실습 과제 설명"
 git push -u origin master
